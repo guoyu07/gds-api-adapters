c83bc00039060a228f3029fc3070269bc41e023b
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-05-22 12:25:53 +0100

    Change the content register test to use pact
    Instead of using the webmock based test helpers

--- test/content_register_test.rb
+++ test/content_register_test.rb
@@ -4,9 +4,10 @@ require 'gds_api/test_helpers/content_register'
 
 describe GdsApi::ContentRegister do
   include GdsApi::TestHelpers::ContentRegister
+  include PactTest
 
   before do
-    @api_adapter = GdsApi::ContentRegister.new(Plek.find("content-register"))
+    @api_adapter = GdsApi::ContentRegister.new('http://localhost:3077')
   end
 
   describe "#put_entry method" do
@@ -18,7 +19,17 @@ describe GdsApi::ContentRegister do
         "base_path" => "/government/organisations/organisation"
       }
 
-      stub_content_register_put_entry(content_id, entry)
+      content_register.given("an empty content register")
+        .upon_receiving("PUT new entry")
+        .with(
+          method: :put, 
+          path: "/entry/#{content_id}", 
+          body: entry
+        )
+        .will_respond_with(
+          status: 201,
+          body: entry.merge(content_id: content_id)
+        )
 
       response = @api_adapter.put_entry(content_id, entry)
       assert_equal 201, response.code

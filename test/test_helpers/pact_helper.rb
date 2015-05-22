33f218c735fab1d0a7c8d8937ceff2bdb5f044f0
David Heath <david.heath@digital.cabinet-office.gov.uk>
2015-05-22 12:24:38 +0100

    Add pact setup code and provider definition
    The provider definition specifies the `content_register` provider which
    runs on port 3077. This is part of the mechanism for stubbing the
    service and recording the interaction with it.

---
+++ test/test_helpers/pact_helper.rb
@@ -0,0 +1,9 @@
+require 'pact/consumer/minitest'
+
+Pact.service_consumer "GDS API Adapters" do
+  has_pact_with "Content register" do
+    mock_service :content_register do
+      port 3077
+    end
+  end
+end
